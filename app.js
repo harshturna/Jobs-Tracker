d9713131bb6ec1cef13206076e0d87f79d96a6fb
Harsh <harshturna15@gmail.com>
2023-07-29 04:15:54 -0500

    remove rate limiter

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const app = express();
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
-const rateLimiter = require("express-rate-limit");
 
 // Swagger
 const swaggerUi = require("swagger-ui-express");
@@ -28,13 +27,6 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.set("trust proxy", 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-  })
-);
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
